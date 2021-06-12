1be27ea9d78a83bfca3c73fc01b37006e3b86c4b
manoj1995madushanka <madushankamanoj414@gmail.com>
2021-06-13 01:45:48 +0530

    add column

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -11,17 +12,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test',// this title used in recent activity popup in phone
+      title: 'test', // this title used in recent activity popup in phone
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.grey,
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.all(10.0),
-            padding: EdgeInsets.all(10.0),
-            child: Text('Hi'),
+          child: Column(
+            mainAxisSize: MainAxisSize.max, // set column size of the screen to its chaild elements total height
+            verticalDirection:VerticalDirection.down , // set direction of child elements
+            mainAxisAlignment: MainAxisAlignment.end,// set position of child elements
+            children: [
+              Container(
+                color: Colors.grey,
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Text('Hi'),
+              ),
+              Container(
+                color: Colors.blue,
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Text('Hi'),
+              ),
+            ],
           ),
         ),
       ),
